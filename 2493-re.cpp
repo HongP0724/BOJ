735790f1b03acdf187e7843f735f1d5e5a035269
HONG\hongs <hongsun724@gmail.com>
2024-09-09 01:15:54 +0900

    주석 추가

--- 2493-re.cpp
+++ 2493-re.cpp
@@ -1,8 +1,12 @@
+//기억 다잡고 스택써서 다시 제대로 풀어봄.
+//옛날코드하고 비교해보니까 거의 비슷하긴함.
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(void)
 {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     stack<pair<int, int>> s;
     int n;
     int max = 0;
